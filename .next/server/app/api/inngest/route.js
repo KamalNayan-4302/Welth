785aba33b71d1b84083c57736762e601f2d3c706
Kamal Nayan Yadav <kamalnayan4302@gmail.com>
2025-06-12 17:32:52 +0530

    Initial Commit

---
+++ .next/server/app/api/inngest/route.js
@@ -0,0 +1,12 @@
+const CHUNK_PUBLIC_PATH = "server/app/api/inngest/route.js";
+const runtime = require("../../../chunks/[turbopack]_runtime.js");
+runtime.loadChunk("server/chunks/node_modules_next_dist_26f140._.js");
+runtime.loadChunk("server/chunks/d7b7b_zod_lib_559a08._.js");
+runtime.loadChunk("server/chunks/node_modules_inngest_f43eed._.js");
+runtime.loadChunk("server/chunks/node_modules_tr46_816df9._.js");
+runtime.loadChunk("server/chunks/node_modules_25c871._.js");
+runtime.loadChunk("server/chunks/[root of the server]__f805d4._.js");
+runtime.loadChunk("server/chunks/74696_@react-email_render_dist_node_index_mjs_83f6ac._.js");
+runtime.loadChunk("server/chunks/_fc945d._.js");
+runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/inngest/route/actions.js { ACTIONS_MODULE0 => \"[project]/actions/send-email.js [app-route] (ecmascript)\" } [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
+module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/inngest/route.js [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
---
+++ README.md
@@ -0,0 +1,2 @@
+# Welth
+Take control of your finances with our sleek Next.js/React dashboard—securely sign in with Clerk, glance your balances, track transactions, and spot trends through interactive charts. Create or edit accounts and transactions in seconds, get real-time alerts, all safely delivered over HTTPS.
